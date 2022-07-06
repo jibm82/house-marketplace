96534a2b98dd372019000a2a7b520bcb77fdab42
Jorge Baez <jibm_82@yahoo.com.mx>
2022-07-05 22:54:59 -0500

    Implement toast alerts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
 
 import Navbar from "./components/Navbar"
 
@@ -8,6 +9,8 @@ import Offers from "./pages/Offers"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
+
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
   return (
@@ -23,6 +26,8 @@ function App() {
         </Routes>
         <Navbar />
       </Router>
+
+      <ToastContainer />
     </>
   );
 }
