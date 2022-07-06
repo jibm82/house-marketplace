ecdc85811554c7ad32ef37a7b07de55811f3f84a
Jorge Baez <jibm_82@yahoo.com.mx>
2022-07-05 22:24:33 -0500

    Implement registration

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
+import { setDoc, doc, serverTimestamp } from "firebase/firestore"
+import { db } from "../firebase.config"
+
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"
 
@@ -21,6 +25,32 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      const userData = {...formData}
+      delete userData.password
+      userData.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, "users", user.uid), userData)
+
+      navigate("/")
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -31,7 +61,7 @@ function SignUp() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
           <input
               type="text"
               className="nameInput"
@@ -76,7 +106,7 @@ function SignUp() {
               <p className="signUpText">
                 Sign Up
               </p>
-              <button className="signUpButton">
+              <button type="submit" className="signUpButton">
                 <ArrowRightIcon fill="#fff" width="34px" height="34px" />
               </button>
             </div>
