114e00a60a1bb562ea2efa1e71752a23312f0978
Jorge Baez <jibm_82@yahoo.com.mx>
2023-09-19 21:16:32 -0600

    Updates

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react"
 import { getAuth, updateProfile } from "firebase/auth"
-import { useNavigate, Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { db } from "../firebase.config"
 import { doc, updateDoc } from "firebase/firestore"
 import { toast } from "react-toastify"
+
+import arrowRightIcon from "../assets/svg/keyboardArrowRightIcon.svg"
+import homeIcon from "../assets/svg/homeIcon.svg"
 
 function Profile() {
   const auth = getAuth()
@@ -89,6 +92,12 @@ function Profile() {
             />
           </form>
         </div>
+
+        <Link to="/create-listing" className="createListing">
+          <img src={homeIcon} alt="home" />
+          <p>Sell or rent your home</p>
+          <img src={arrowRightIcon} alt="arrow right" />
+        </Link>
       </main>
     </div>
   )
